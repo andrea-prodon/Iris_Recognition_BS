6312314d9528d81c3efe7e78e6630a686ba7f127
andrea <salsedine98@gmail.com>
2022-12-04 17:40:20 +0100

    Segmentation updated

--- Segmentation/segmentation.py
+++ Segmentation/segmentation.py
@@ -2,24 +2,42 @@ import cv2
 import numpy as np
 import os
 
-path = "Dataset/CASIA1/2/002_1_1.jpg"
+path = "Dataset/CASIA1/7/007_1_1.jpg"
 image_read = cv2.imread(path)
 output = image_read.copy()
+cv2.imshow("original", image_read)
+
+# CANNY
+
 image_test = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 image_test = cv2.GaussianBlur(image_test, (7, 7), 1)
-cv2.imshow("imagetest", image_test)
-image_test = cv2.Canny(image_test, 80, 100, apertureSize=3)
-cv2.imshow("image_test", image_test)
+image_test = cv2.Canny(image_test, 20, 70, apertureSize=3)
+cv2.imshow("canny", image_test)
 
-circles = cv2.HoughCircles(image_test,cv2.HOUGH_GRADIENT,1,50,
-                            param1=50,param2=50,minRadius=0,maxRadius=0)
+# IRIS CIRCLE
+
+hough_circle = cv2.HoughCircles(image_test, cv2.HOUGH_GRADIENT, 1.3, 800)
+if hough_circle is not None:
+    hough_circle = np.round(hough_circle[0, :]).astype("int")
+    for (x, y, radius) in hough_circle:
+        cv2.circle(output, (x, y), radius, (255, 0, 0), 4)
+
+# CANNY 2
+
+image_test = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+image_test = cv2.GaussianBlur(image_test, (7, 7), 1)
+image_test = cv2.Canny(image_test, 100, 120, apertureSize=3)
+cv2.imshow("canny-2", image_test)
+
+# PUPIL CIRCLE
+
+circles = cv2.HoughCircles(image_test,cv2.HOUGH_GRADIENT, 1, 800,
+                            param1=50, param2=20, minRadius=0, maxRadius=60)
 circles = np.round(circles[0, :]).astype("int")
  
 for (x, y, r) in circles:
-		# draw the circle in the output image, then draw a rectangle
-		# corresponding to the center of the circle
 		cv2.circle(output, (x, y), r, (0, 255, 0), 2)
-		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+		cv2.rectangle(output, (x - 2, y - 2), (x + 2, y + 2), (0, 128, 255), -1)
 
 cv2.imshow('detected circles',output)
 cv2.waitKey(0)
